adea993c12846610aac4d5724cd92bb946e41bcd
dipu7388 <dheerendra.mcs16.du@gmail.com>
2023-09-06 01:25:17 +0530

    first change to company

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+        Bamur Industries & Engineering Private Limited
+        </h1>
+
+
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  className="App-link"
+  href="tel:+919044991559"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Call For Enquiries (+91-904-499-1559)
+</a>
+<a
+  className="App-link"
+  href="mailto:bamurindustries@gmail.com"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Email us:  bamurindustries@gmail.com
+</a>
+<p>
+Bamur Industries & Engineering Private Limited is a dynamic and innovative company on the rise, and we are thrilled to announce the launch of our brand-new website. As a forward-thinking organization, we've always been committed to excellence in engineering and industry solutions. Our website serves as a digital gateway to showcase our cutting-edge products, services, and our unwavering dedication to customer satisfaction. With this platform, we aim to provide a seamless and informative experience for our valued clients, partners, and stakeholders. Whether you're seeking state-of-the-art engineering solutions, industry insights, or simply want to connect with us, our website is your one-stop destination. We look forward to embarking on this exciting online journey with you and sharing our passion for innovation and engineering excellence. Welcome to the new era of Bamur Industries & Engineering Private Limited!
+</p>
+
       </header>
     </div>
   );
