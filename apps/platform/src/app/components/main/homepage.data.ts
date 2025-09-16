5cb491b91d255bca46e75862b39fdeb7483425e0
dheerendrasingh2 <dheerendra.singh@si-eam.com>
2025-09-16 17:05:38 +0530

    Image updated

--- apps/platform/src/app/components/main/homepage.data.ts
+++ apps/platform/src/app/components/main/homepage.data.ts
@@ -1,150 +1,100 @@
-import { Image } from "@bamur/core";
+import { Image } from '@bamur/core';
 
 export const E_BIKE_IMAGES: Image[] = [
   {
     url: 'assets/e-bike-1.png',
     alt: 'E-Bike 1',
-    legend: 'E-Bike 1'
+    legend: 'E-Bike 1',
   },
-  {
-    url: 'assets/e-bike-2.png',
-    alt: 'E-Bike 2',
-    legend: 'E-Bike 2'
-  },
-  {
-    url: 'assets/e-bike-3.png',
-    alt: 'E-Bike 3',
-    legend: 'E-Bike 3'
-  },
-  {
-    url: 'assets/scooter.png',
-    alt: 'E-Bike 4',
-    legend: 'E-Bike 4'
-  },
-  {
-    url: 'assets/scooter-1.png',
-    alt: 'E-Bike 5',
-    legend: 'E-Bike 6'
-  }
 ];
 
 export const E_CYCLE_IMAGES: Image[] = [
   {
-    url: 'assets/e-cycle-1.png',
-    alt: 'E-Cycle 1',
-    legend: 'E-Cycle 1'
-  },
-  {
-    url: 'assets/e-cycle-2.png',
-    alt: 'E-Cycle 2',
-    legend: 'E-Cycle 2'
-  },
-  {
-    url: 'assets/e-cycle-3.png',
-    alt: 'E-Cycle 3',
-    legend: 'E-Cycle 3'
-  },
-  {
-    url: 'assets/e-cycle-4.png',
-    alt: 'E-Cycle 4',
-    legend: 'E-Cycle 4'
-  },
-  {
-    url: 'assets/e-cycle-5.png',
-    alt: 'E-Cycle 5',
-    legend: 'E-Cycle 5'
-  },
-  {
     url: 'assets/ecycle-1.png',
-    alt: 'E-Cycle 6',
-    legend: 'E-Cycle 6'
+    alt: 'E-Cycle 1',
+    legend: 'E-Cycle 1',
   },
-  {
-    url: 'assets/ecycle.png',
-    alt: 'E-Cycle 7',
-    legend: 'E-Cycle 7'
-  }
 ];
 
 export const E_RIKSHA_IMAGES: Image[] = [
   {
     url: 'assets/e-riksha-1.jpg',
     alt: 'E-Riksha 1',
-    legend: 'E-Riksha 1'
+    legend: 'E-Riksha 1',
   },
   {
     url: 'assets/e-riksha-2.jpg',
     alt: 'E-Riksha 2',
-    legend: 'E-Riksha 2'
+    legend: 'E-Riksha 2',
   },
   {
     url: 'assets/e-riksha-3.jpg',
     alt: 'E-Riksha 3',
-    legend: 'E-Riksha 3'
+    legend: 'E-Riksha 3',
   },
   {
     url: 'assets/e-riksha-4.jpg',
     alt: 'E-Riksha 4',
-    legend: 'E-Riksha 4'
+    legend: 'E-Riksha 4',
   },
   {
     url: 'assets/e-riksha-5.jpg',
     alt: 'E-Riksha 5',
-    legend: 'E-Riksha 5'
+    legend: 'E-Riksha 5',
   },
   {
     url: 'assets/e-riksha-6.jpg',
     alt: 'E-Riksha 6',
-    legend: 'E-Riksha 6'
+    legend: 'E-Riksha 6',
   },
   {
     url: 'assets/e-riksha-7.jpg',
     alt: 'E-Riksha 7',
-    legend: 'E-Riksha 7'
+    legend: 'E-Riksha 7',
   },
   {
     url: 'assets/e-riksha-8.jpg',
     alt: 'E-Riksha 8',
-    legend: 'E-Riksha 8'
+    legend: 'E-Riksha 8',
   },
   {
     url: 'assets/e-riksha-9.jpg',
     alt: 'E-Riksha 9',
-    legend: 'E-Riksha 9'
+    legend: 'E-Riksha 9',
   },
   {
     url: 'assets/e-riksha-10.jpg',
     alt: 'E-Riksha 10',
-    legend: 'E-Riksha 10'
+    legend: 'E-Riksha 10',
   },
   {
     url: 'assets/e-riksha-11.jpg',
     alt: 'E-Riksha 11',
-    legend: 'E-Riksha 11'
+    legend: 'E-Riksha 11',
   },
   {
     url: 'assets/e-riksha-12.jpg',
     alt: 'E-Riksha 12',
-    legend: 'E-Riksha 12'
+    legend: 'E-Riksha 12',
   },
   {
     url: 'assets/e-riksha-13.jpg',
     alt: 'E-Riksha 13',
-    legend: 'E-Riksha 13'
+    legend: 'E-Riksha 13',
   },
   {
     url: 'assets/e-riksha-14.jpg',
     alt: 'E-Riksha 14',
-    legend: 'E-Riksha 14'
+    legend: 'E-Riksha 14',
   },
   {
     url: 'assets/e-riksha-15.jpg',
     alt: 'E-Riksha 15',
-    legend: 'E-Riksha 15'
+    legend: 'E-Riksha 15',
   },
   {
     url: 'assets/e-riksha-16.jpg',
     alt: 'E-Riksha 16',
-    legend: 'E-Riksha 16'
-  }
+    legend: 'E-Riksha 16',
+  },
 ];
